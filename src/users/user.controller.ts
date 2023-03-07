6fc561f1accfe0ba4ce1b59e932e0bd03871c060
hieu <hieuqbh15@gmail.com>
2023-03-07 22:27:23 +0700

    Pipe, Class Tranform, Class Validator

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import console from "console";
+import { UserDto } from "./user.dto";
 
 @Controller('users')
 export class UserController {
+    @UsePipes(new ValidationPipe())
     @Get()
     getallusers(){
         return [{
@@ -13,10 +17,17 @@ export class UserController {
         }]
     }
     @Post()
-    createuser(){
-        return{
-            name: 'hieu',
-            age: '18',
-        }
+    createuser(@Body() user:UserDto):UserDto{
+        user.createdAt=new Date();
+        user.id=1;
+        user.updatedAt= new Date();
+        // const userReal=plainToClass(UserDto,user,{excludeExtraneousValues:true});
+        // console.log(userReal);
+       return UserDto.plainToClass(user);
+    }
+    @Get(':id')
+    getUserById(@Param('id') id:number){
+        console.log(id);
+        return 'test'
     }
 }
---
+++ src/users/user.dto.ts
@@ -0,0 +1,19 @@
+import { Expose ,Transform} from "class-transformer";
+import { IsNotEmpty,Length } from "class-validator";
+import { BaseDto } from "src/base.dto";
+
+
+
+export class UserDto extends  BaseDto{
+    @IsNotEmpty()
+    @Expose()
+    username: string;
+    firstName:string;
+    lastName:string;
+    @Transform(({obj})=> obj.firstName + " " + obj.lastName)
+    fullname:string;
+    @IsNotEmpty()
+    @Length(10,20)
+    @Expose()
+    password: string;
+}
